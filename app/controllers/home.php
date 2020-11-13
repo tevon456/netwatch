b95667e728924dea2ebbb1a6436777ee7762b4a2
tevon <tevondavisjc@gmail.com>
2020-11-13 04:30:54 -0500

    tested views in home controller

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,6 +4,9 @@ class Home extends Controller
 {
     public function index($name = '')
     {
-        echo $name;
+        $user = $this->model('user');
+        $user->name = $name;
+
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
