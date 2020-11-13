5c2dbf7bad1f63979ecfbaecb51bfdedc17c9ef1
tevon <tevondavisjc@gmail.com>
2020-11-13 04:32:09 -0500

    new home view

---
+++ app/views/home/index.php
@@ -0,0 +1,19 @@
+<div>
+    <nav class="bp3-navbar .modifier bp3-dark">
+        <div class="bp3-navbar-group bp3-align-left">
+            <div class="bp3-navbar-heading">Netwatch</div>
+        </div>
+        <div class="bp3-navbar-group bp3-align-right">
+            <button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
+            <button class="bp3-button bp3-minimal bp3-icon-document">Dashboard</button>
+            <span class="bp3-navbar-divider"></span>
+            <button class="bp3-button bp3-minimal bp3-icon-user"></button>
+        </div>
+    </nav>
+    <div class="p-8">
+        <div class="bp3-card .modifier">
+            Hello <?= $data['name'] ?>
+        </div>
+    </div>
+
+    <div>
