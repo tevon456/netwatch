11c11103d4736ede8ed9ca712b229d45defde202
tevon <tevondavisjc@gmail.com>
2020-11-13 01:18:22 -0500

    documentation

--- app/core/app.php
+++ app/core/app.php
@@ -1,13 +1,25 @@
 <?php
 
+/**
+ * Main app class
+ * ...
+ */
 class App
 {
     protected $controller = 'home';
     protected $method = 'index';
     protected $params = [];
 
+    /**
+     * Responsible for checking and useing the correct controller based on the url
+     * 
+     */
     public function __construct()
     {
+
+        /* given a url /home/index/3 the code below evaluates the /home section
+           of the url and initializes the the home controller doing checks to ensure said controller exists
+        */
         $url = $this->parseUrl();
         if (file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
@@ -15,8 +27,11 @@ class App
         }
         require_once '../app/controllers/' . $this->controller . '.php';
 
+        /* given a url /home/index/3 the code below evaluates the /index section
+           of the url and calls the method index of home controller doing checks
+           to ensure said said exists
+        */
         $this->controller = new $this->controller;
-        print_r($this->controller);
 
         if (isset($url[1])) {
             if (method_exists($this->controller, $url[1])) {
@@ -24,8 +39,19 @@ class App
                 unset($url[1]);
             }
         }
+
+        $this->params = $url ? array_values($url) : [];
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
+    /**
+     * Parses the url
+     *
+     * @return  array  containing the parsed url parts
+     * ...
+     * 
+     */
     public function parseUrl()
     {
         if (isset($_GET['url'])) {
