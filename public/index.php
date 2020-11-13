0f22e2ea253dbf9f5935677ffd6d7b3caf0be538
tevon <tevondavisjc@gmail.com>
2020-11-13 01:19:08 -0500

    add basic html

--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
-<?php
-require '../vendor/autoload.php';
-require_once '../app/init.php';
-$app = new App();
+<head>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <!-- Style dependencies -->
+    <link href="https://unpkg.com/normalize.css@^7.0.0" rel="stylesheet" />
+    <!-- Blueprint stylesheets -->
+    <link href="https://unpkg.com/@blueprintjs/icons@^3.4.0/lib/css/blueprint-icons.css" rel="stylesheet" />
+    <link href="https://unpkg.com/@blueprintjs/core@^3.10.0/lib/css/blueprint.css" rel="stylesheet" />
+    <style>
+        body {
+            background-color: #ececec;
+        }
+    </style>
+</head>
+
+<body class="mt-2 text-gray-600">
+    <?php
+    require '../vendor/autoload.php';
+    require_once '../app/init.php';
+    $app = new App();
+    ?>
+</body>
