02adabacc747a1d36a09ef72b67c9262b330fe62
tevon <tevondavisjc@gmail.com>
2020-11-16 19:04:32 -0500

    database init

--- app/init.php
+++ app/init.php
@@ -1,4 +1,7 @@
 <?php
+// require_once 'seed.php';
 require_once 'core/app.php';
 require_once 'core/controller.php';
-require '../vendor/autoload.php';
+require_once 'database.php';
+require_once '../vendor/autoload.php';
+//auth method on the main controller that checks session
