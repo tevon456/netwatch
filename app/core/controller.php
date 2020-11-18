4b5f156197ddf999432cc51623c0e570ece8cc04
tevon <tevondavisjc@gmail.com>
2020-11-18 02:16:07 -0500

    new auth based methods

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use Laminas\Crypt\Password\Bcrypt;
+
 class Controller
 {
     public function model($model)
@@ -11,5 +14,78 @@ class Controller
     public function view($view, $data = [])
     {
         require_once '../app/views/' . $view . '.php';
+    }
+
+    /**
+     * Authenticates the user using the credentials passed as params
+     *
+     * @return  bool  true if successful
+     * 
+     * 
+     */
+    public function authenticate($email, $password)
+    {
+        return $this->authenticator($email, $password);
+    }
+
+    private function authenticator($email, $password)
+    {
+        //prepare bcrypt
+        $bcrypt = new Bcrypt();
+        //Query the user by email
+        $user = User::where('email', $email)->first();
+
+        //Check if a user was returned from the query
+        if ($user) {
+            //hash and compare provided password to hashed password in DB
+            if ($bcrypt->verify($password,  $user->password)) {
+                session_start();
+                $sessionPayload = ['name' => $user->name, 'email' => $user->email];
+                $_SESSION['AUTH'] = $sessionPayload;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * returns the logged in user's associative array from the global session variable, use isAuthenticated() before running this method to check if there is a user;
+     *
+     * @return  array
+     * 
+     * 
+     */
+    public function authenticatedUser()
+    {
+        //returns the user associative array from the global session variable
+        session_start();
+        if (isset($_SESSION['AUTH'])) {
+            return $_SESSION['AUTH'];
+        }
+    }
+
+    /**
+     * Checks if the user is authenticated
+     *
+     * @return  bool 
+     * 
+     * 
+     */
+    public function isAuthenticated()
+    {
+        if (isset($_SESSION['AUTH'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function endUserSession()
+    {
+        session_unset();
+        session_destroy();
     }
 }
