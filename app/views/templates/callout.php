29d2ba48f33c79f1339fdd45638ac5602e45559a
tevon <tevondavisjc@gmail.com>
2020-11-19 18:20:03 -0500

    callout now takes a class for type

--- app/views/templates/callout.php
+++ app/views/templates/callout.php
@@ -1,4 +1,4 @@
-<div class="bp3-callout bp3-intent-primary">
+<div class="bp3-callout <?= $data['type'] ? $data['type'] : 'bp3-intent-primary' ?>">
     <h4 class="bp3-heading"> <?= $data['title'] ?></h4>
     <?= $data['body'] ?>
 </div>
