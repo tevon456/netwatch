f696533b69f0db3efd5f867f9c374342af9610c4
tevon <tevondavisjc@gmail.com>
2020-11-19 18:20:36 -0500

    new dropdown menu

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,15 +4,31 @@
             <div class="bp3-navbar-heading">Netwatch</div>
         </div>
         <div class="bp3-navbar-group bp3-align-right">
-            <button class="bp3-button bp3-minimal bp3-icon-home">Home</button>
-            <button class="bp3-button bp3-minimal bp3-icon-document">Dashboard</button>
+            <a href="./../home/index" role="button" class="bp3-button bp3-icon-home" tabindex="0">Home</a>
+            <span class="mr-2"></span>
+            <a href="./../home/index" role="button" class="bp3-button bp3-icon-application" tabindex="0">Dashboard</a>
+            <span class="mr-2"></span>
+            <a href="./../home/index" role="button" class="bp3-button bp3-icon-pie-chart" tabindex="0">Reports</a>
+            <span class="mr-2"></span>
+            <a href="./../home/index" role="button" class="bp3-button bp3-icon-people" tabindex="0">Users</a>
             <span class="bp3-navbar-divider"></span>
-            <button class="bp3-button bp3-minimal bp3-icon-user"></button>
+            <div>
+                <a href="./../home/index" role="button" class="bp3-button bp3-minimal bp3-icon-user" tabindex="0" title="">
+                </a>
+                <ul class="bp3-menu bp3-elevation-1 netwatchMenu">
+                    <li class="bp3-menu-header">
+                        <h6 class="bp3-heading"><?= $data['user']['name'] ?></h6>
+                    </li>
+                    <li><a href="./../home/index" role="button" class="bp3-menu-item bp3-icon-layout-circle" tabindex="0">
+                            Account</a></li>
+                    <li><a href="./../auth/logout" role="button" class="bp3-menu-item bp3-icon-power" tabindex="0">
+                            Signout</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
     <div class="p-8 mt-4">
         <div class="grid grid-cols-3 place-items-stretch h-48 ">
-
             <?php
             foreach ($data['data'] as $value) {
                 echo
