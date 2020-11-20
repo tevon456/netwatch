95c45003c79884c334f924cadf05b722b425890f
tevon <tevondavisjc@gmail.com>
2020-11-19 20:40:09 -0500

    db setup in init.php

--- app/init.php
+++ app/init.php
@@ -1,7 +1,6 @@
 <?php
-// require_once 'seed.php';
+require_once 'seed.php';
 require_once 'core/app.php';
 require_once 'core/controller.php';
 require_once 'database.php';
 require_once '../vendor/autoload.php';
-//auth method on the main controller that checks session
