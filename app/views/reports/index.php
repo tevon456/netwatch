c9a1427c24676838dadac43e83182d4498ee1762
tevon <tevondavisjc@gmail.com>
2020-12-10 01:46:24 -0500

    remove search bar

---
+++ app/views/reports/index.php
@@ -0,0 +1,35 @@
+<div class="p-8 mt-4">
+
+    <div class="bp3-card mb-4">
+        <div class="bp3-input-group .modifier">
+            <span class="bp3-icon bp3-icon-search"></span>
+            <input type="text" class="bp3-input bp3-round bp3-fill" placeholder="Search" />
+            <button class="bp3-button bp3-minimal bp3-intent-primary bp3-icon-arrow-right"></button>
+        </div>
+    </div>
+
+    <div class="">
+        <div class="flex justify-between">
+            <form id="form" class="php-form bp3-card" style="width: 230px;" method="post" action="<?php $_PHP_SELF ?>">
+                <h3 class="bp3-heading">Total users</h3>
+                <p class="text-5xl font-bold text-green-400 tabular-nums">34</p>
+            </form>
+            <form id="form" class="php-form bp3-card" style="width: 230px;" method="post" action="<?php $_PHP_SELF ?>">
+                <h3 class="bp3-heading">Update user</h3>
+                <p class="text-5xl font-bold text-blue-500 tabular-nums">34</p>
+            </form>
+            <form id="form" class="php-form bp3-card" style="width: 230px;" method="post" action="<?php $_PHP_SELF ?>">
+                <h3 class="bp3-heading">Update user</h3>
+                <p class="text-5xl font-bold text-yellow-500 tabular-nums">34</p>
+            </form>
+            <form id="form" class="php-form bp3-card" style="width: 230px;" method="post" action="<?php $_PHP_SELF ?>">
+                <h3 class="bp3-heading">Update user</h3>
+                <p class="text-5xl font-bold text-yellow-500 tabular-nums">34</p>
+            </form>
+            <form id="form" class="php-form bp3-card" method="post" action="<?php $_PHP_SELF ?>">
+                <h3 class="bp3-heading">Update user</h3>
+                <p class="text-5xl font-bold text-yellow-500 tabular-nums">34</p>
+            </form>
+        </div>
+    </div>
+</div>
